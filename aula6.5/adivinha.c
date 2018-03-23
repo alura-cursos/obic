ea245f84b0780a0c6f4e993f7520a1c909796c23
mayra.oliveira <mayra.oliveira@alura.com.br>
2018-03-23 20:33:08 +0000

    Projeto da aula 6 - Operações, matemática e ponto flutuante

---
+++ aula6.5/adivinha.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define JOGANDO 1
+#define ACABOU 0
+
+int main() {
+
+	// imprime mensagem de boas vindas
+	printf("*******************************************\n");
+	printf("* Bem-vindo ao nosso jogo de adivinhação! *\n");
+	printf("*******************************************\n");
+
+	// jogo
+	int numerosecreto = 42;
+
+	//printf("O número secreto é %d.\n", numerosecreto);
+
+	int tentativa = 1;
+	double pontos = 1000;
+	int situacao = JOGANDO;
+
+	while(situacao == JOGANDO){
+
+		printf("\nTentativa %d\n", tentativa);
+
+		int chute;
+		printf("Qual é o seu chute (entre 1 e 100)? ");
+		scanf("%d", &chute);
+
+		int chuteehinvalido = (chute < 1) || (chute > 100);
+ 		if(chuteehinvalido) {
+			printf("O seu chute deve estar entre 1 e 100.\n");
+			continue;
+		}
+
+		printf("O seu chute é %d.\n", chute);
+
+		int acertou = (chute == numerosecreto);
+
+		if(acertou) {
+			printf("Parabéns! Você acertou!\n");
+			printf("Que tal jogar novamente?\n");
+			situacao = ACABOU;
+			continue;
+		}
+
+		int maior = (numerosecreto > chute);
+		if(maior) {
+			printf("O número secreto é maior do que %d.\n", chute);
+		} else {
+			printf("O número secreto é menor do que %d.\n", chute);
+		}
+
+		double pontosPerdidos = abs(chute - numerosecreto) / 2.0;
+		// printf("Voce pedeu %f pontos.\n", pontosPerdidos);
+		pontos -= pontosPerdidos;
+
+		tentativa++;
+
+	}	
+
+	printf("Fim do jogo com %d tentativas e %.2f pontos!\n", tentativa, pontos);
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
