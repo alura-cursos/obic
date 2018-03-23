8b4ddced5bd970bcea448c887f2e58ccb896debb
mayra.oliveira <mayra.oliveira@alura.com.br>
2018-03-23 20:32:51 +0000

    Projeto da aula 3 - Controle de fluxo e diversos chutes

---
+++ aula3.4/adivinha.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main() {
+
+	// imprime mensagem de boas vindas
+	printf("*******************************************\n");
+	printf("* Bem-vindo ao nosso jogo de adivinhação! *\n");
+	printf("*******************************************\n");
+
+	// jogo
+	int numerosecreto = 42;
+
+	//printf("O número secreto é %d.\n", numerosecreto);
+
+	for(int tentativa = 1; tentativa <= 3; tentativa++){
+
+		printf("\nTentativa %d de 3\n", tentativa);
+
+		int chute;
+		printf("Qual é o seu chute? ");
+		scanf("%d", &chute);
+
+		printf("O seu chute é %d.\n", chute);
+
+		int acertou = (chute == numerosecreto);
+		if(acertou) {
+			printf("Parabéns! Você acertou!\n");
+			printf("Que tal jogar novamente?\n");
+			break;
+		} else {
+			printf("Que pena, você errou dessa vez!\n");
+			printf("Não desanime, tente novamente!\n");
+
+			int maior = (numerosecreto > chute);
+			if(maior){
+				printf("O número secreto é maior do que %d.\n", chute);
+			} else {
+				printf("O número secreto é menor do que %d.\n", chute);
+			}
+		}
+	}	
+
+	printf("Fim do jogo!\n");
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
